854845bda4d868d5deee9509c32fc1889fa93683
anthony <anthony.chinwo@gmail.com>
2018-03-01 21:20:12 +0000

    - disable api server temporarily

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ const options = {
 
 const server = new WebpackDevServer(webpack(config), options);
 
-require('./api_server') // start api server
+//require('./api_server') // start api server
 
 server.listen(port, 'localhost', function (err) {
     if (err) {
